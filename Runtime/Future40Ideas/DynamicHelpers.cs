0084898693abcf8b8f8c227abdba831371f44d6f
Darren Cauthon <darren@cauthon.com>
2011-09-03 06:37:54 -0500

    Add ToDynamicInstance helper method.

---
+++ Runtime/Future40Ideas/DynamicHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace TechTalk.SpecFlow.Future40Ideas
+{
+    public static class DynamicHelpers
+    {
+        public static DynamicTable ToDynamicInstance(this Table table)
+        {
+            return new DynamicTable(table);
+        }
+    }
+}
---
+++ Tests/RuntimeTests/Future40Ideas/DynamicHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow.Future40Ideas;
+
+namespace TechTalk.SpecFlow.RuntimeTests.Future40Ideas
+{
+    [TestFixture]
+    public class DynamicHelperTests
+    {
+        [Test]
+        public void ToDynamicInstance_returns_a_dynamic_instance_of_the_table()
+        {
+            var table = new Table("x", "y");
+            table.AddRow("FirstName", "Howard");
+            table.AddRow("LastName", "Roark");
+
+            dynamic dynamicInstance = table.ToDynamicInstance();
+
+            Assert.IsNotNull(dynamicInstance);
+            Assert.AreEqual("Howard", dynamicInstance.FirstName);
+            Assert.AreEqual("Roark", dynamicInstance.LastName);
+        }
+    }
+}
